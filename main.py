baec150af819c013717d8fe94c3bafe1c5c9b94b
Arya Boudaie <aboudaie@brandeis.edu>
2017-03-20 22:54:28 -0400

    Got it to convert properly!

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 # For now this is where I am just putting a scratch pad for my progress
 
+import verovio
+tk = verovio.toolkit(False)
+
+tk.setResourcePath(r"C:\Users\misin\OneDrive\Documents\GitHub\verovio\data")
+tk.setNoLayout(True)
+tk.setFormat("mei")
+tk.loadFile("vos_pastores_MENSURAL.mei")
+tk.renderToSvgFile("test.svg", 1)
